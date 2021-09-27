c2f9833d07effec8524d2450455ed8eb5918387d
kitavia <91342060+kitavia@users.noreply.github.com>
2021-09-27 13:26:49 -0400

    Basic version checker

--- PlayIdleHelper.js
+++ PlayIdleHelper.js
@@ -1,91 +1,100 @@
 'use strict';
-const version = '0.1';
+(async () => {
+  const version = chrome.runtime.getManifest().version;
+  const flagOutdated = await isOutdated();
 
-const minimumEnergy = 10; // the script will not leave the user with less than this amount of energy
+  const panelElements = document.getElementsByClassName('col-md-4 padUp');
+  const energy = parseInt(document.getElementsByClassName('nav-item')[4].innerText);
 
-const panelElements = document.getElementsByClassName('col-md-4 padUp');
-const energy = parseInt(document.getElementsByClassName('nav-item')[4].innerText);
-const workButton = document.getElementsByClassName('btn btn-secondary padUp begColor pulse')[0];
+  var url = location.href;
+  var urlFileName = url.substring(url.lastIndexOf('/') + 1);
 
-var url = location.href;
-var urlFileName = url.substring(url.lastIndexOf('/') + 1);
+  const minimumEnergy = 10; // the script will not leave the user with less than this amount of energy
 
-function Initialize() {
-  const navBar = document.getElementsByClassName('navbar-nav ml-auto')[0];
-  const navItems = document.getElementsByClassName('nav-item');
+  function Initialize() {
+    let displayVersion = version;
+    if (flagOutdated) displayVersion = '(Outdated)';
 
-  //TODO: Replace with HTML
-  const toClone = navItems[2];
-  const newElem = toClone.cloneNode(true);
+    const navBar = document.getElementsByClassName('navbar-nav ml-auto')[0];
+    const navItems = document.getElementsByClassName('nav-item');
 
-  const aTag = newElem.getElementsByClassName('nav-link js-scroll-trigger navBadge')[0];
-  aTag.innerHTML = '<i class="fa fa-heart pulse clanColor"></i> PlayIdleHelper ' + version;
-  aTag.href = 'https://github.com/Kitavia/PlayIdleHelper';
-  aTag.target = '_blank';
+    //TODO: Replace with HTML
+    const toClone = navItems[2];
+    const newElem = toClone.cloneNode(true);
 
-  navBar.insertBefore(newElem, navItems[navItems.length - 2]);
-}
+    const aTag = newElem.getElementsByClassName('nav-link js-scroll-trigger navBadge')[0];
+    aTag.innerHTML = '<i class="fa fa-heart pulse clanColor"></i> PlayIdleHelper' + ' ' + displayVersion;
+    aTag.href = 'https://github.com/Kitavia/PlayIdleHelper';
+    aTag.target = '_blank';
 
-function Run() {
-  if (urlFileName.startsWith('dashboard.php')) {
-    if (energy > minimumEnergy) {
-      // TODO: Comply with rules when they exist.
-      ClickWorkButton();
+    navBar.insertBefore(newElem, navItems[navItems.length - 2]);
+  }
+
+  function Run() {
+    if (urlFileName.startsWith('dashboard.php')) {
+      if (energy > minimumEnergy) {
+        // TODO: Comply with rules when they exist.
+        ClickWorkButton();
+      }
+
+      CalculatePropertyProfit();
     }
 
-    CalculatePropertyProfit();
-  }
+    //TODO: Filtering options
+    if (urlFileName.startsWith('jobs.php')) {
+      RemoveUnwatedJobs();
+    }
 
-  //TODO: Filtering options
-  if (urlFileName.startsWith('jobs.php')) {
-    RemoveUnwatedJobs();
-  }
-
-  if (urlFileName.startsWith('properties.php')) {
-    CalculateRoi();
-  }
-}
-
-function CalculateRoi() {
-  for (const element of panelElements) {
-    const output = element.getElementsByClassName('btn btn-primary mb-2 btn-block padUp btn-md')[0];
-    const profit = parseInt(element.getElementsByClassName('moneyColor')[0].innerHTML.split('$')[1].replace(',', ''));
-    const cost = parseInt(output.innerHTML.split('$')[1].replace(',', ''));
-    const roi = Math.ceil(cost / profit);
-    const roiDays = Math.ceil((roi * 12) / 24);
-
-    output.innerHTML = output.innerHTML + '<br>ROI in days = ' + roiDays;
-  }
-}
-
-function RemoveUnwatedJobs() {
-  for (const element of panelElements) {
-    const h4 = element.getElementsByTagName('H4');
-    if (h4[0].outerText != 'Surgeon') {
-      element.style.display = 'none';
+    if (urlFileName.startsWith('properties.php')) {
+      CalculateRoi();
     }
   }
-}
 
-function ClickWorkButton() {
-  workButton.click();
-}
+  function CalculateRoi() {
+    for (const element of panelElements) {
+      const output = element.getElementsByClassName('btn btn-primary mb-2 btn-block padUp btn-md')[0];
+      const profit = parseInt(element.getElementsByClassName('moneyColor')[0].innerHTML.split('$')[1].replace(',', ''));
+      const cost = parseInt(output.innerHTML.split('$')[1].replace(',', ''));
+      const roi = Math.ceil(cost / profit);
+      const roiDays = Math.ceil((roi * 12) / 24);
 
-function CalculatePropertyProfit() {
-  const propertiesPanel = document.getElementsByClassName('col-6 col-md-3 mb-5 text-center')[2];
-  const income = parseInt(propertiesPanel.getElementsByClassName('mapColor')[2].innerText.split(' ')[0].replace('$', '').replace(',', ''));
-  const cost = parseInt(propertiesPanel.getElementsByClassName('redColor')[0].innerText.split(' ')[0].replace('$', '').replace(',', ''));
-  const profit = (income - cost).toLocaleString();
+      output.innerHTML = output.innerHTML + '<br>ROI in days = ' + roiDays;
+    }
+  }
 
-  const panel = propertiesPanel.getElementsByClassName('panel-body text-center')[0];
-  panel.insertAdjacentHTML('beforeend', `<br><i class="fa fa-equals moneyColor"></i><text class="mapColor"> $${profit} / 12hrs</text>`);
-}
+  function RemoveUnwatedJobs() {
+    for (const element of panelElements) {
+      const h4 = element.getElementsByTagName('H4');
+      if (h4[0].outerText != 'Surgeon') {
+        element.style.display = 'none';
+      }
+    }
+  }
 
-function GetVersion() {
-  fetch('manifest.json').then((data) => {
-    return data.version;
-  });
-}
+  function ClickWorkButton() {
+    const workButton = document.getElementsByClassName('btn btn-secondary padUp begColor pulse')[0];
 
-Initialize();
-Run();
+    workButton.click();
+  }
+
+  function CalculatePropertyProfit() {
+    const propertiesPanel = document.getElementsByClassName('col-6 col-md-3 mb-5 text-center')[2];
+    const income = parseInt(propertiesPanel.getElementsByClassName('mapColor')[2].innerText.split(' ')[0].replace('$', '').replace(',', ''));
+    const cost = parseInt(propertiesPanel.getElementsByClassName('redColor')[0].innerText.split(' ')[0].replace('$', '').replace(',', ''));
+    const profit = (income - cost).toLocaleString();
+
+    const panel = propertiesPanel.getElementsByClassName('panel-body text-center')[0];
+    panel.insertAdjacentHTML('beforeend', `<br><i class="fa fa-equals moneyColor"></i><text class="mapColor"> $${profit} / 12hrs</text>`);
+  }
+
+  async function isOutdated() {
+    const manifest = await fetch('https://raw.githubusercontent.com/Kitavia/PlayIdleHelper/main/manifest.json');
+    const json = await manifest.json();
+
+    if (version >= json.version) return false;
+    else return true;
+  }
+
+  Initialize();
+  Run();
+})();
--- readme.md
+++ readme.md
@@ -1,8 +1,7 @@
 # PlayIdleHelper
 
-PlayIdleHelper is a userscript intended to provide quality of life improvements to [PlayIdle.com](https://www.playidle.com/)
-Since the game seems dead and isn't holding my interest well, I'm not going to make this an actual extension.
-Which means it won't automatically update etc.
+PlayIdleHelper is a learning project & extension intended to provide quality of life improvements to [PlayIdle.com](https://www.playidle.com/)
+It is currently not published as an actual extension for multiple reasons, but maybe in the future.
 
 ## Current Features
 
@@ -10,6 +9,7 @@ Which means it won't automatically update etc.
 - Calculates ROI for properties
 - Filter jobs (currently hides all jobs except surgeon)
 - Automatically spend energy (down to 10)
+- Checks for updates automatically (but doesn't apply them yet)
 
 ## Installation
 
@@ -19,9 +19,7 @@ Might not work on non-chromium based browsers, haven't tested and probably won't
 1. Download this repo
 2. Unzip it anywhere
 3. Go to [chrome://extensions](chrome://extensions)
-4. Click "load unpacked extension"
-5. Select the directory of the extension
-6. Done!
+4. Load the folder you unzipped as an unpacked extension
 
 ## Contributing
 
