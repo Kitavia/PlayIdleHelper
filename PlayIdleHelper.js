a79916a1c75249fd8069c63add8b520688560337
kitavia <91342060+kitavia@users.noreply.github.com>
2021-09-26 20:41:01 -0400

    Initial commit

---
+++ PlayIdleHelper.js
@@ -0,0 +1,91 @@
+'use strict';
+const version = '0.1';
+
+const minimumEnergy = 10; // the script will not leave the user with less than this amount of energy
+
+const panelElements = document.getElementsByClassName('col-md-4 padUp');
+const energy = parseInt(document.getElementsByClassName('nav-item')[4].innerText);
+const workButton = document.getElementsByClassName('btn btn-secondary padUp begColor pulse')[0];
+
+var url = location.href;
+var urlFileName = url.substring(url.lastIndexOf('/') + 1);
+
+function Initialize() {
+  const navBar = document.getElementsByClassName('navbar-nav ml-auto')[0];
+  const navItems = document.getElementsByClassName('nav-item');
+
+  //TODO: Replace with HTML
+  const toClone = navItems[2];
+  const newElem = toClone.cloneNode(true);
+
+  const aTag = newElem.getElementsByClassName('nav-link js-scroll-trigger navBadge')[0];
+  aTag.innerHTML = '<i class="fa fa-heart pulse clanColor"></i> PlayIdleHelper ' + version;
+  aTag.href = 'https://www.google.com/';
+  aTag.target = '_blank';
+
+  navBar.insertBefore(newElem, navItems[navItems.length - 2]);
+}
+
+function Run() {
+  if (urlFileName.startsWith('dashboard.php')) {
+    if (energy > minimumEnergy) {
+      // TODO: Comply with rules when they exist.
+      ClickWorkButton();
+    }
+
+    CalculatePropertyProfit();
+  }
+
+  //TODO: Filtering options
+  if (urlFileName.startsWith('jobs.php')) {
+    RemoveUnwatedJobs();
+  }
+
+  if (urlFileName.startsWith('properties.php')) {
+    CalculateRoi();
+  }
+}
+
+function CalculateRoi() {
+  for (const element of panelElements) {
+    const output = element.getElementsByClassName('btn btn-primary mb-2 btn-block padUp btn-md')[0];
+    const profit = parseInt(element.getElementsByClassName('moneyColor')[0].innerHTML.split('$')[1].replace(',', ''));
+    const cost = parseInt(output.innerHTML.split('$')[1].replace(',', ''));
+    const roi = Math.ceil(cost / profit);
+    const roiDays = Math.ceil((roi * 12) / 24);
+
+    output.innerHTML = output.innerHTML + '<br>ROI in days = ' + roiDays;
+  }
+}
+
+function RemoveUnwatedJobs() {
+  for (const element of panelElements) {
+    const h4 = element.getElementsByTagName('H4');
+    if (h4[0].outerText != 'Surgeon') {
+      element.style.display = 'none';
+    }
+  }
+}
+
+function ClickWorkButton() {
+  workButton.click();
+}
+
+function CalculatePropertyProfit() {
+  const propertiesPanel = document.getElementsByClassName('col-6 col-md-3 mb-5 text-center')[2];
+  const income = parseInt(propertiesPanel.getElementsByClassName('mapColor')[2].innerText.split(' ')[0].replace('$', '').replace(',', ''));
+  const cost = parseInt(propertiesPanel.getElementsByClassName('redColor')[0].innerText.split(' ')[0].replace('$', '').replace(',', ''));
+  const profit = (income - cost).toLocaleString();
+
+  const panel = propertiesPanel.getElementsByClassName('panel-body text-center')[0];
+  panel.insertAdjacentHTML('beforeend', `<br><i class="fa fa-equals moneyColor"></i><text class="mapColor"> $${profit} / 12hrs</text>`);
+}
+
+function GetVersion() {
+  fetch('manifest.json').then((data) => {
+    return data.version;
+  });
+}
+
+Initialize();
+Run();
---
+++ readme.md
@@ -0,0 +1,28 @@
+# PlayIdleHelper
+
+PlayIdleHelper is a userscript intended to provide quality of life improvements to [PlayIdle.com](https://www.playidle.com/)
+Since the game seems dead and isn't holding my interest well, I'm not going to make this an actual extension.
+Which means it won't automatically update etc.
+
+## Current Features
+
+- Calculates total property income
+- Calculates ROI for properties
+- Filter jobs (currently hides all jobs except surgeon)
+- Automatically spend energy (down to 10)
+
+## Installation
+
+This installs as an unpacked browser extension, the below example is for chrome
+Might not work on non-chromium based browsers, haven't tested and probably won't
+
+1. Download this repo
+2. Unzip it anywhere
+3. Go to [chrome://extensions](chrome://extensions)
+4. Click "load unpacked extension"
+5. Select the directory of the extension
+6. Done!
+
+## Contributing
+
+All changes will comply with the site's rules
